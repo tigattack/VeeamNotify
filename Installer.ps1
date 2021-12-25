4fd7e5109d8d3ac8060f15d8ec3bc4cb0a12c366
tigattack <10629864+tigattack@users.noreply.github.com>
2021-12-25 01:12:03 +0000

    feat(installer): rewrite for VeeamNotify

--- Installer.ps1
+++ Installer.ps1
@@ -3,8 +3,14 @@
 # Prepare variables
 $rootPath = 'C:\VeeamScripts'
 $project = 'VeeamNotify'
-$webhookRegex = 'https:\/\/(.*\.)?discord(app)?\.com\/api\/webhooks\/([^\/]+)\/([^\/]+)'
-$mentionOnWarnExist = $mentionOnFailExist = $true
+
+Write-Output @"
+#######################################
+#                                     #
+#        VeeamNotify Installer        #
+#                                     #
+#######################################`n`n
+"@
 
 # Get latest release from GitHub
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -100,110 +106,80 @@ Remove-Item -Path "$env:TEMP\$project-$release.zip"
 # Get config
 $config = Get-Content "$rootPath\$project\config\conf.json" -Raw | ConvertFrom-Json
 
-# Check mention keys exist
-if ($null -eq $config.mention_on_warning) {
-	$mentionOnWarnExist = $false
-}
-if ($null -eq $config.mention_on_fail) {
-	$mentionOnFailExist = $false
-}
-
-# Check user has webhook URL ready
-do {
-	$webhookPrompt = Read-Host -Prompt 'Do you have your Discord webhook URL ready? Y/N'
-}
-until ($webhookPrompt -in 'Y', 'N')
-
-# Prompt user to create webhook first if not ready
-If ($webhookPrompt -eq 'N') {
-	Write-Output 'Please create a Discord webhook before continuing.'
-	Write-Output 'Full instructions available at https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks'
-
-	# Prompt user to launch URL
-	$launchPrompt = Read-Host -Prompt 'Open URL? Y/N'
-	If ($launchPrompt -eq 'Y') {
-		Start-Process 'https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks'
-	}
-
-	exit
-}
-
 # Prompt user with config options
 do {
-	$webhookUrl = Read-Host -Prompt 'Please enter your Discord webhook URL'
-	If ($webhookUrl -notmatch $webhookRegex) {
-		Write-Output "`nInvalid webhook URL. Please try again."
-	}
+	$service = Read-Host -Prompt "Which service do you wish to send notifications to?`n1 = Discord`n2 = Slack`n3 = Teams"
 }
-until ($webhookUrl -match $webhookRegex)
+until ($service -in '1', '2', '3')
 
-If ($mentionOnWarnExist) {
-	Write-Output "`nDo you wish to be mentioned in Discord when a job fails or finishes with warnings?"
-
-	do {
-		$mentionPreference = Read-Host -Prompt "1 = No`n2 = On warn`n3 = On fail`n4 = On fail and on warn`nYour choice"
-		If (1..4 -notcontains $mentionPreference) {
-			Write-Output "`nInvalid choice. Please try again."
-		}
+Switch ($service) {
+	1 {
+		$config.service = 'Discord'
+		$userIdMessage = 'Please enter your Discord user ID'
 	}
-	until (1..4 -contains $mentionPreference)
-
-	If ($mentionPreference -ne 1) {
-		do {
-			try {
-				[Int64]$userId = Read-Host -Prompt "`nPlease enter your Discord user ID"
-			}
-			catch [System.Management.Automation.ArgumentTransformationMetadataException] {
-				Write-Output "`nInvalid user ID. Please try again."
-			}
-		}
-		until ($userId.ToString().Length -gt 1)
+	2 {
+		$config.service = 'Slack'
+		$userIdMessage = 'Please enter your Slack member ID'
 	}
-
-	# Set config values
-	$config.webhook = $webhookUrl
-	Switch ($mentionPreference) {
-		1 {
-			$config.mention_on_fail = $false
-			$config.mention_on_warning = $false
-		}
-		2 {
-			$config.mention_on_fail = $false
-			$config.mention_on_warning = $true
-			$config.userId = $userId
-		}
-		3 {
-			$config.mention_on_fail = $true
-			$config.mention_on_warning = $false
-			$config.userId = $userId
-		}
-		4 {
-			$config.mention_on_fail = $true
-			$config.mention_on_warning = $true
-			$config.userId = $userId
-		}
+	3 {
+		$config.service = 'Teams'
+		$userIdMessage = 'Please enter your Teams email address'
 	}
 }
 
-elseif ($mentionOnFailExist) {
-	do {
-		$mentionPreference = Read-Host -Prompt 'Do you wish to be mentioned in Discord when a job finishes in a failed state? Y/N'
+$config.webhook = Read-Host -Prompt 'Please enter your webhook URL'
+
+Write-Output @"
+`nDo you wish to be mentioned/tagged when a job fails or finishes with warnings?
+1 = No
+2 = On warn
+3 = On fail
+4 = On fail and on warn
+"@
+
+do {
+	$mentionPreference = Read-Host -Prompt 'Your choice'
+	If (1..4 -notcontains $mentionPreference) {
+		Write-Output 'Invalid choice. Please try again.'
 	}
-	until ($mentionPreference -in 'Y', 'N')
+}
+until (1..4 -contains $mentionPreference)
 
-	# Set config values
-	If ($mentionPreference -eq 'Y') {
-		do {
-			$config.mention_on_fail = $true
+If ($mentionPreference -ne 1) {
+	do {
+		$userId = Read-Host -Prompt "`n$userIdMessage"
+	}
+	until ($userId.ToString().Length -gt 1)
 
-			try {
-				[Int64]$config.userId = Read-Host -Prompt "`nPlease enter your Discord user ID"
-			}
-			catch [System.Management.Automation.ArgumentTransformationMetadataException] {
-				Write-Output "`nInvalid user ID. Please try again."
-			}
-		}
-		until ($userId.ToString().Length -gt 1)
+	If ($config.service -eq 3) {
+		Write-Output "Teams also requires a name to be specified for mentions.`nIf you don't enter your name, your username (from your email address) will be used.`nIf you'd prefer this, type nothing and press enter."
+		$config.teams_user_name = Read-Host -Prompt 'Please enter your name on Teams'
+	}
+	Else {
+		$config.teams_user_name = ''
+	}
+}
+
+# Set config values
+Switch ($mentionPreference) {
+	1 {
+		$config.mention_on_fail = $false
+		$config.mention_on_warning = $false
+	}
+	2 {
+		$config.mention_on_fail = $false
+		$config.mention_on_warning = $true
+		$config."$($config.service)_user_id" = $userId
+	}
+	3 {
+		$config.mention_on_fail = $true
+		$config.mention_on_warning = $false
+		$config."$($config.service)_user_id" = $userId
+	}
+	4 {
+		$config.mention_on_fail = $true
+		$config.mention_on_warning = $true
+		$config."$($config.service)_user_id" = $userId
 	}
 }
 
@@ -211,6 +187,7 @@ elseif ($mentionOnFailExist) {
 Try {
 	Write-Output "`nSetting configuration..."
 	ConvertTo-Json $config | Set-Content "$rootPath\$project\config\conf.json"
+	Write-Output "`nConfiguration set successfully. Configuration can be found in `"$rootPath\$project\config\conf.json`"."
 }
 catch {
 	Write-Warning "Failed to write configuration file at `"$rootPath\$project\config\conf.json`". Please open the file and complete configuration manually."
