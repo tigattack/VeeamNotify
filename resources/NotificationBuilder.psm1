01619ea51452a196bd440e2f28d5f8d68c6d88d6
tigattack <10629864+tigattack@users.noreply.github.com>
2021-12-28 11:19:57 +0000

    feat(notifbuilder): minor Slack embed styling

--- resources/NotificationBuilder.psm1
+++ resources/NotificationBuilder.psm1
@@ -609,7 +609,7 @@ function New-SlackPayload {
 				type      = 'section'
 				text      = @{
 					type = 'mrkdwn'
-					text = "*$JobName*`nSession result: $Status`nJob type: $JobType"
+					text = "*$JobName*`nSession result: $Status`n`nJob type: $JobType"
 				}
 				accessory = @{
 					type      = 'image'
