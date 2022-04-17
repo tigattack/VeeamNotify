aeda27fe29451617a9b815ec1a1cd04b0cb8ed9e
tigattack <10629864+tigattack@users.noreply.github.com>
2022-04-17 21:18:41 +0100

    test(installer): add installer tests

---
+++ tests/Installer.Tests.ps1
@@ -0,0 +1,78 @@
+Describe 'Installer.ps1' {
+	BeforeAll {
+		# Define project name
+		$project = 'VeeamNotify'
+
+		# Create temp install dir
+		$installDir = New-Item -Path (Join-Path -Path $PSScriptRoot -ChildPath 'test-install') -Type Directory -Force
+
+		# Get installer path
+		$installerPath = Get-ChildItem -Path (Split-Path -Path $PSScriptRoot -Parent) -Filter 'Installer.ps1'
+
+		# Define installer params
+		$installerParams = @{
+			NonInteractive    = $true
+			InstallParentPath = $installDir
+		}
+
+		# Define required files
+		$expectedFiles = @(
+			'Bootstrap.ps1',
+			'AlertSender.ps1'
+		)
+		$expectedFiles += $(
+			foreach ($dir in 'resources', 'config') {
+				$files = Get-ChildItem -Path "$(Split-Path -Path $PSScriptRoot -Parent)\$dir"
+				foreach ($file in $files) {
+					[string](Join-Path -Path $file.Directory.Name -ChildPath $file.Name)
+				}
+			}
+		)
+
+		# Define required files check
+		[scriptblock]$expectedFilesCheck = {
+			foreach ($file in $expectedFiles) {
+				Join-Path -Path "$installDir\$project" -ChildPath $file | Should -Exist
+			}
+		}
+
+		# Get releases
+		[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+		$releases = Invoke-RestMethod -Uri "https://api.github.com/repos/tigattack/$project/releases" -Method Get
+	}
+
+	Context 'NonInteractive Version Install' {
+		It 'Should install from specific version' -Skip:(-not $releases) {
+			# Run installer
+			. $installerPath -Version 'v1.0' @installerParams
+
+			# Check for expected files
+			Invoke-Command -ScriptBlock $expectedFilesCheck
+		}
+	}
+
+	Context 'NonInteractive Latest Install' {
+		It 'Should install from latest release' -Skip:(-not $releases) {
+			# Run installer
+			. $installerPath -Latest Release @installerParams
+
+			# Check for expected files
+			Invoke-Command -ScriptBlock $expectedFilesCheck
+		}
+	}
+
+	Context 'NonInteractive Branch Install' {
+		It 'Should install from branch' {
+			# Run installer
+			. $installerPath -Branch dev @installerParams
+
+			# Check for expected files
+			Invoke-Command -ScriptBlock $expectedFilesCheck
+		}
+	}
+
+	AfterAll {
+		# Remove temp install dir
+		Remove-Item -Path $installDir -Recurse -Force
+	}
+}
