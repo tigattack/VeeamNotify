d083816e106dee1d89c2bab214645708b78d5d47
tigattack <10629864+tigattack@users.noreply.github.com>
2022-06-23 16:37:21 +0100

    fix(updateinfo): do no quit if GH API error
    and still return minimal info

--- resources/UpdateInfo.psm1
+++ resources/UpdateInfo.psm1
@@ -11,53 +11,60 @@ function Get-UpdateStatus {
 		}
 		catch {
 			$versionStatusCode = $_.Exception.Response.StatusCode.value__
-			Write-LogMessage -Tag 'ERROR' -Message "Failed to query GitHub for the latest version. Please check your internet connection and try again. Status code: $versionStatusCode"
-			exit 1
+			Write-LogMessage -Tag 'WARN' -Message "Failed to query GitHub for the latest version. Please check your internet connection and try again. Status code: $versionStatusCode"
 		}
 
-		# Get latest stable
-		foreach ($i in $releases) {
-			if (-not $i.prerelease) {
-				$latestStable = $i.tag_name
-				break
+		If ($releases) {
+			# Get latest stable
+			foreach ($i in $releases) {
+				if (-not $i.prerelease) {
+					$latestStable = $i.tag_name
+					break
+				}
+			}
+
+			# Get latest prerelease
+			foreach ($i in $releases) {
+				if ($i.prerelease) {
+					$latestPrerelease = $i.tag_name
+					break
+				}
+			}
+
+			# Determine if prerelease
+			$prerelease = $false
+			foreach ($i in $releases) {
+				if ($i.tag_name -eq $currentVersion -and $i.prerelease) {
+					$prerelease = $true
+					break
+				}
+			}
+
+			# Set version status
+			If ($currentVersion -gt $latestStable) {
+				$status = 'Ahead'
+			}
+			elseif ($currentVersion -lt $latestStable) {
+				$status = 'Behind'
+			}
+			else {
+				$status = 'Current'
+			}
+
+			# Create PSObject to return.
+			$out = New-Object PSObject -Property @{
+				CurrentVersion   = $currentVersion
+				LatestStable     = $latestStable
+				LatestPrerelease = $latestPrerelease
+				Prerelease       = $prerelease
+				Status           = $status
 			}
 		}
-
-		# Get latest prerelease
-		foreach ($i in $releases) {
-			if ($i.prerelease) {
-				$latestPrerelease = $i.tag_name
-				break
+		Else {
+			# Create PSObject to return.
+			$out = New-Object PSObject -Property @{
+				CurrentVersion = $currentVersion
 			}
-		}
-
-		# Determine if prerelease
-		$prerelease = $false
-		foreach ($i in $releases) {
-			if ($i.tag_name -eq $currentVersion -and $i.prerelease) {
-				$prerelease = $true
-				break
-			}
-		}
-
-		# Set version status
-		If ($currentVersion -gt $latestStable) {
-			$status = 'Ahead'
-		}
-		elseif ($currentVersion -lt $latestStable) {
-			$status = 'Behind'
-		}
-		else {
-			$status = 'Current'
-		}
-
-		# Create PSObject to return.
-		$out = New-Object PSObject -Property @{
-			CurrentVersion   = $currentVersion
-			LatestStable     = $latestStable
-			LatestPrerelease = $latestPrerelease
-			Prerelease       = $prerelease
-			Status           = $status
 		}
 
 		# Return PSObject.
