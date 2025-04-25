87685540177b6b8a2e1128401edaf5e6ae47b220
tigattack <10629864+tigattack@users.noreply.github.com>
2025-04-25 15:57:08 +0100

    refactor(vbrsessioninfo): implement model for return object, improve param validation

--- resources/VBRSessionInfo.psm1
+++ resources/VBRSessionInfo.psm1
@@ -1,59 +1,54 @@
+class VBRSessionInfo {
+	[ValidateNotNullOrEmpty()][Veeam.Backup.Core.CBackupSession]$Session
+	[ValidateNotNullOrEmpty()][string]$JobName
+}
+
 function Get-VBRSessionInfo {
+	[OutputType([VBRSessionInfo])]
 	param (
-		[Parameter(Mandatory=$true)]$SessionId,
-		[Parameter(Mandatory=$true)]$JobType
+		[Parameter(Mandatory)][ValidateNotNullOrEmpty()]
+		[string]$SessionId,
+		[Parameter(Mandatory)][ValidateNotNullOrEmpty()]
+		[string]$JobType
 	)
 
 	# Import VBR module
 	Import-Module Veeam.Backup.PowerShell -DisableNameChecking
 
-	if (($null -ne $SessionId) -and ($null -ne $JobType)) {
+	switch ($JobType) {
+		# VM job
+		{$_ -in 'Backup', 'Replica'} {
 
-		# Switch on job type.
-		switch ($JobType) {
+			# Get the session details.
+			$session = Get-VBRBackupSession | Where-Object {$_.Id.Guid -eq $SessionId}
 
-			# VM job
-			{$_ -in 'Backup','Replica'} {
-
-				# Get the session details.
-				$session = Get-VBRBackupSession | Where-Object {$_.Id.Guid -eq $SessionId}
-
-				# Get the job's name from the session details.
-				$jobName = $session.Name
-			}
-
-			# Agent job
-			{$_ -in 'EpAgentBackup','BackupToTape','FileToTape'} {
-				# Fetch current session to load .NET module
-				# It appears some of the underlying .NET items are lazy-loaded, so this is necessary
-				# to load in whatever's required to utilise the GetByOriginalSessionId method.
-				# See https://forums.veeam.com/powershell-f26/want-to-capture-running-jobs-by-session-type-i-e-sobr-tiering-t75583.html#p486295
-				Get-VBRSession -Id $SessionId | Out-Null
-				# Get the session details.
-				$session = [Veeam.Backup.Core.CBackupSession]::GetByOriginalSessionId($SessionId)
-
-				# Copy the job's name to it's own variable.
-				if ($JobType -eq 'EpAgentBackup') {
-					$jobName = $job.Info.Name
-				}
-				elseif ($JobType -in 'BackupToTape','FileToTape') {
-					$jobName = $job.Name
-				}
-			}
+			# Get the job's name from the session details.
+			$jobName = $session.Name
 		}
 
-		# Create PSObject to return.
-		New-Object PSObject -Property @{
-			Session = $session
-			JobName = $jobName
+		# Agent or tape job
+		{$_ -in 'EpAgentBackup', 'BackupToTape', 'FileToTape'} {
+			# Fetch current session to load .NET module
+			# It appears some of the underlying .NET items are lazy-loaded, so this is necessary
+			# to load in whatever's required to utilise the GetByOriginalSessionId method.
+			# See https://forums.veeam.com/powershell-f26/want-to-capture-running-jobs-by-session-type-i-e-sobr-tiering-t75583.html#p486295
+			Get-VBRSession -Id $SessionId | Out-Null
+			# Get the session details.
+			$session = [Veeam.Backup.Core.CBackupSession]::GetByOriginalSessionId($SessionId)
+
+			# Copy the job's name to it's own variable.
+			if ($JobType -eq 'EpAgentBackup') {
+				$jobName = $job.Info.Name
+			}
+			elseif ($JobType -in 'BackupToTape', 'FileToTape') {
+				$jobName = $job.Name
+			}
 		}
 	}
 
-	elseif ($null -eq $SessionId) {
-		Write-LogMessage -Tag 'WARN' -Message 'SessionId is null.'
-	}
-
-	elseif ($null -eq $JobType) {
-		Write-LogMessage -Tag 'WARN' -Message 'JobType is null.'
+	# Create PSObject to return.
+	return [VBRSessionInfo]@{
+		Session = $session
+		JobName = $jobName
 	}
 }
