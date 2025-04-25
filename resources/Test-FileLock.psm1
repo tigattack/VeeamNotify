23e1a639cb4e890792e34f8575bfc40039134639
tigattack <10629864+tigattack@users.noreply.github.com>
2025-04-25 15:47:58 +0100

    refactor(test-filelock): require string input, not `string[]`, other misc. refactoring

--- resources/Test-FileLock.psm1
+++ resources/Test-FileLock.psm1
@@ -1,32 +1,35 @@
+# I believe the source of the original implementation of this function (since changed) was:
+# https://mcpmag.com/articles/2018/07/10/check-for-locked-file-using-powershell.aspx
+# Written by Boe Prox, Microsoft MVP.
 function Test-FileLock {
-	[cmdletbinding()]
+	[CmdletBinding()]
+	[OutputType([PSCustomObject])]
 	param (
-		[parameter(Mandatory = $True, ValueFromPipeline = $True, ValueFromPipelineByPropertyName = $True)]
+		[Parameter(Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
 		[Alias('FullName', 'PSPath')]
-		[string[]]$Path
+		[string]$Path
 	)
 	process {
-		foreach ($Item in $Path) {
-			#Ensure this is a full path
-			$Item = Convert-Path $Item
-			#Verify that this is a file and not a directory
-			if ([System.IO.File]::Exists($Item)) {
-				try {
-					$FileStream = [System.IO.File]::Open($Item, 'Open', 'Write')
-					$FileStream.Close()
-					$FileStream.Dispose()
-					$IsLocked = $False
-				}
-				catch [System.UnauthorizedAccessException] {
-					$IsLocked = 'AccessDenied'
-				}
-				catch {
-					$IsLocked = $True
-				}
-				[pscustomobject]@{
-					File     = $Item
-					IsLocked = $IsLocked
-				}
+		# Ensure this is a full path
+		$Path = Convert-Path $Path
+		# Verify that this is a file and not a directory
+		if ([System.IO.File]::Exists($Path)) {
+			try {
+				$FileStream = [System.IO.File]::Open($Path, 'Open', 'Write')
+				$FileStream.Close()
+				$FileStream.Dispose()
+				$IsLocked = $False
+			}
+			catch [System.UnauthorizedAccessException] {
+				$IsLocked = 'AccessDenied'
+			}
+			catch {
+				$IsLocked = $True
+			}
+
+			[pscustomobject]@{
+				File     = $Item
+				IsLocked = $IsLocked
 			}
 		}
 	}
